4cb34de9425d291c4734622a4f2ee93003597c61
sasi <sasi@sasi-PC>
2021-02-11 16:56:07 +0530

    first commit

---
+++ GitDemo/src/main/java/com/GitDemo/HelloGit/WelcomeGit.java
@@ -0,0 +1,10 @@
+package com.GitDemo.HelloGit;
+
+public class WelcomeGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi Git, tis is my first Demo");
+	}
+
+}
